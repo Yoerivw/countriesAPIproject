d41864fca1c5d6e501d4cb5f46991a0a59b50d4c
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2019-11-04 22:34:20 +0200

    extracted necessary data, pushed into objects

--- js/app.js
+++ js/app.js
@@ -6,9 +6,16 @@ const countriesList = [];
 const printCountries = () => {
 
     for (let i = 0; i < countriesList.length; i++) {
-        let country = countriesList[i];
+        let country = countriesList[i].country,
+
+            capital = countriesList[i].capital,
+            continent = countriesList[i].continent,
+            subRegion = countriesList[i].subRegion,
+            population = countriesList[i].population,
+            flag = countriesList[i].flag;
         let li = document.createElement('li');
-        li.innerHTML = country;
+        let output = '<h3>Country: ' + country + '</h3><p> Capital: ' + capital + '</p><p> Continent: ' + continent + '</p><p> Sub-continent' + subRegion + '</p><p> Population: ' + population + '</p><img src="' + flag + '">'
+        li.innerHTML = output;
         countryList.appendChild(li);
     }
 }
@@ -32,24 +39,42 @@ const getData = () => {
             console.log(xhr.response);
         } else if (xhr.status == 200) {
             const data = xhr.response;
+            /*  let countryData = {
+                 country: data.name,
+                 capital: data.capital,
+                 continent: data.region,
+                 subContinent: data['sub-region'],
+                 population: data.population,
+                 flag: data.flag
+             } */
+
             for (let i = 0; i < data.length; i++) {
-                countriesList.push(data[i].name);
+                dataC = data[i];
+                let country = dataC.name,
+                    capital = dataC.capital,
+                    continent = dataC.region,
+                    subRegion = dataC.subregion,
+                    population = dataC.population,
+                    flag = dataC.flag;
+                /*  let countryData = {
+                     country: country,
+                     capital = capital,
+                     continent: continent,
+                     subcontinent: subContinent,
+                     population: population,
+                     flag: flag
+                 } */
+
+
+                countriesList.push({ country, capital, continent, subRegion, population, flag });
             }
             console.log(countriesList);
 
         }
-        //one way of parsing the JSON data
-        //const data = JSON.parse(xhr.response);
 
 
     }
-
-
-
-
-
     xhr.send();
-
 }
 
 
